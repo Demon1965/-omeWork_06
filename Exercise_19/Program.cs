7108a335b5f76e7eb6b1443b083ea498e997e13a
Demon1965 <dmitry.korzhenevsky@yandex.ru>
2023-02-19 12:26:37 +0300

    HomeWork_03

---
+++ Exercise_19/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string str = Console.ReadLine();
+    int num = Convert.ToInt32(str);
+    return num;
+}
+
+int num=Promt("Please, insert the five-digit number: ");
+    if (10000<=num && num<=99999)
+    {   
+        int res=0;
+        int res1=num;
+        while(res1>=1)
+        {
+            res=res*10+res1%10;
+            res1=res1/10;
+        }
+            if (num==res)
+            {
+                Console.WriteLine("The number you've inserted is PALINDROME!");
+            }
+                else
+            {
+                Console.WriteLine("The number you've inserted is USUAL number!");
+            }
+    }
+    else
+    {
+        Console.WriteLine("You're wrong! Try again");
+    }
---
+++ Exercise_21/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double Distance3D(double ax, double ay, double az, double bx, double by, double bz)
+{
+double distance = Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
+return Math.Round(distance, 2);
+}
+
+Console.Write("Please, insert coordinate Х of point А: ");
+double Ax = Convert.ToDouble(Console.ReadLine());
+Console.Write("Please, insert coordinyte Y of point А: ");
+double Ay = Convert.ToDouble(Console.ReadLine());
+Console.Write("Please, insert coordinate Z of point А: ");
+double Az = Convert.ToDouble(Console.ReadLine());
+Console.Write("Please, insert coordinate Х of point B: ");
+double Bx = Convert.ToDouble(Console.ReadLine());
+Console.Write("Please, insert coordinate Y of point B: ");
+double By = Convert.ToDouble(Console.ReadLine());
+Console.Write("Please, insert coordinate Z of point B: ");
+double Bz = Convert.ToDouble(Console.ReadLine());
+
+double result = Distance3D(Ax, Ay, Az, Bx, By, Bz);
+System.Console.WriteLine($"The distance between these points in 3D will be:  {result}");
